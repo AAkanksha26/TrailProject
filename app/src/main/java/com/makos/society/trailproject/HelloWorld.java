df3db5050ab7e42064d27b08a3abc0788f9529c3
PC1 <AAkanksha26>
2018-09-05 13:07:09 +0530

    ok

---
+++ app/src/main/java/com/makos/society/trailproject/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.makos.society.trailproject;
+
+import android.util.Log;
+
+/**
+ * Created by PC1 on 05-09-2018.
+ */
+
+public class HelloWorld
+{
+ public static void main(String args)
+ {
+     System.out.println("Hello World");
+ }
+}
